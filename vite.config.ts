66f2a8aa4843bd269b5a6eac73fc0e9cfa3bde49
Erik Lundström <hello@eriklundstrom.com>
2024-11-11 23:27:14 +0100

    feat: update base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), mkcert()],
   assetsInclude: ['**/*.glb'],
-  base: '/secret-santa-v2/',
+  base: '/',
   css: {
     modules: {
       localsConvention: 'camelCase',
