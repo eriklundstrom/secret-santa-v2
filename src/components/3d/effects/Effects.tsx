15b494c05d2f7e5c5fc7ce34b750169b763e19d5
Erik Lundström <hello@eriklundstrom.com>
2024-11-12 01:47:06 +0100

    feat: add effect

--- src/components/3d/effects/Effects.tsx
+++ src/components/3d/effects/Effects.tsx
@@ -1,18 +1,15 @@
-import { EffectComposer, ToneMapping } from '@react-three/postprocessing'
+import { EffectComposer, N8AO, ToneMapping } from '@react-three/postprocessing'
 
 function Effects() {
-  /**
-   * <N8AO
-   *         halfRes
-   *         aoSamples={5}
-   *         aoRadius={0.4}
-   *         distanceFalloff={0.75}
-   *         intensity={1}
-   *       />
-   */
-
   return (
     <EffectComposer stencilBuffer>
+      <N8AO
+        halfRes
+        aoSamples={5}
+        aoRadius={0.4}
+        distanceFalloff={0.75}
+        intensity={1}
+      />
       <ToneMapping />
     </EffectComposer>
   )
